e896c0e79d4be780a5e7339608779a7571df2a89
Leke <leke.dobruna@gmail.com>
2023-08-03 13:17:59 +0200

    Added default plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,34 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { "xiyaowong/transparent.nvim", lazy = false, opts = {} },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { "mini.surround", opts = {}, lazy = false },
+  { "ayu-theme/ayu-vim", lazy = false },
+  { "mini.move", opts = {}, lazy = false },
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = { colorcolumn = 120, disabled_filetypes = { "help" } },
+  },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
