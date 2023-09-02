779bc2391a9d588264c26876cf6418200cb44181
Leke <leke.dobruna@gmail.com>
2023-09-02 16:10:18 +0200

    Add customizations for Ayu theme

---
+++ highlights/ayu.lua
@@ -0,0 +1,8 @@
+return { -- a table of overrides/changes to the ayu theme
+  Normal = { bg = "#000000" },
+  CursorLine = { bg = "#131313" },
+  LineNr = { bg = "#000000", fg = "#5C6773" },
+  Pmenu = { bg = "#131313" },
+  SignColumn = { bg = "#000000" },
+  FloatBorder = { bg = "#000000" },
+}
